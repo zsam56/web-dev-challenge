16bee1117bba46416c7e2b1ef77aeefe59df3ec0
Isaiah Smith <jordankid93@gmail.com>
2017-07-13 16:42:10 -0400

    Initial project files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Returns if a given phrase is a palindrome
+ *
+ * @param {string} phrase
+ * @return {bool} if [phrase] is a palindrome
+ *
+ */
+function IsItAPalindrome(phrase) {
+
+}
+
+$(document).ready(function(){
+	
+});

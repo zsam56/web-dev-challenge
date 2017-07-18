db524a3f5595ab856849b7f4de835945d69e54ad
Isaiah Smith <jordankid93@gmail.com>
2017-07-18 11:31:17 -0400

    Challenge ready

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # web-dev-challenge
 A simple web development challenge for potential hires
+
+### Part 0
+#### Setup
+
+Please fork this repository to your own github, clone your personal repo, and complete the tasks there
+
+### Part 1 (Required)
+#### Making it work
+
+Typing a phrase into the text input and hitting the ENTER key or clicking the "Check" button should result in correctly letting you know if the entered phrase is a palindrome or not.
+
+For this to work correctly you must implement the code for 2 functions, RemoveWhitespace and IsItAPalindrome.
+
+Once you think you have it, enter some phrases and test it out yourself.
+
+In addition to your own tests you can click the Test button in the lower left hand corner of the page OR run "Text()" in the console to see how you do against some tests we've come up with.
+
+Your solution should pass all tests, run with no errors, and have useful comments where your feel necessary!
+
+NOTE: We expect you to be able to complete this challenge without the need for any addition JavaScript libraries (that means NO jQuery). However, if you use any online references please include links in your comments to the source.
+
+### Part 2 (Optional)
+#### Making it better
+
+Here is where you can feel free to improve on the look, feel, and functionality of the site as you please (provided you stick to not using any additional JavaScript libraries).
+
+### Part 3
+#### Submission
+
+To submit, push all your work and changes to your own repo and create a pull request to the original repository's master branch. Be sure to include any and all additional changes you might have made either in code comments, your ReadMe file, or in an addition .txt file.
--- js/main.js
+++ js/main.js
@@ -1,14 +1,324 @@
+
+/**
+ * Returns a phrase with all whitespace removed
+ *
+ * @param phrase {string} the phrase to have whitespace removed from
+ *
+ * @return {string} [phrase] without any whitespace
+ *
+ */
+function RemoveWhitespace(phrase) {
+	// TODO
+	return phrase;
+}
+
 /**
  * Returns if a given phrase is a palindrome
  *
- * @param {string} phrase
+ * @param phrase {string} the phrase to be checked
+ *
  * @return {bool} if [phrase] is a palindrome
  *
  */
 function IsItAPalindrome(phrase) {
-
+	// TODO
+	return false;
 }
 
-$(document).ready(function(){
+/**
+ * Shows feedback on the page of if the entered phrase is a palidrome or not
+ *
+ * @param wasPalidrome {bool} if the phrase is a palidrome or not
+ *
+ * @return {void}
+ *
+ */
+function ShowFeedback(wasPalidrome) {
+
+	// Grab a reference to the alerts
+	let success = document.getElementById("success");
+	let error = document.getElementById("error");
+
+	// Hide both alerts
+	Hide(success);
+	Hide(error);
+
+	// Show correct alert based on outcome
+	if (wasPalidrome) {
+		Fade(success, 1, 2000);
+	}
+	else {
+		Fade(error, 1, 2000);
+	}
+}
+
+/**
+ * Handles when the form is submitted
+ *
+ * @param e {event} the event object
+ *
+ * @return {void}
+ *
+ */
+function FormSubmitted(e) {
+
+	// NOTE: Don't actually submit the form
+	e.preventDefault();
+
+	// Get the contents of the phrase input
+	let phraseInput = document.getElementById("phrase-input");
+	let phrase = phraseInput.value;
+
+	// Remove the whitespace from the phrase
+	phrase = RemoveWhitespace(phrase);
+
+	// See if its a palidrome
+	let isPalidrome = IsItAPalindrome(phrase);
+
+	// Setup the page to show results
+	ShowFeedback(isPalidrome);
+}
+
+/**
+ * Fades a given element to an opacity
+ *
+ * @param element {element} the element to fade
+ * @param opacity {number} the end opacity to fade to. Rounded to be either 0 or 1. Applies "display:none" if opacity is 0 and "display:block" if not 0
+ * @param duration {number} how long the fade should take (in miliseconds)
+ * @param completeCallback {function} the callback function to be called when the fade is complete
+ *
+ * @return {void}
+ *
+ */
+function Fade(element, opacity, duration, completeCallback) {
+
+	// Clamp opacity
+	opacity = Math.round(Clamp(opacity));
 	
+	// Get the element's starting opacity
+	let startingOpacity = parseFloat(window.getComputedStyle(element).opacity);
+
+	// Return if opacity is already the same change
+	if (startingOpacity == opacity) {
+		// Trigger Callback
+		if (completeCallback != null) {
+			completeCallback();
+		}
+		return;
+	}
+
+	// Get range for opacity change
+	let opacityRange = opacity - startingOpacity;
+
+	// Get time for each individual step (assume we want 100 steps)
+	let stepTime = duration / 100;
+
+	// Get the amount to change the opacity every step
+	let opacityStep = opacityRange / stepTime;
+
+	// Setup interval for transition
+	let interval = setInterval(function(){
+
+		// Get current opacity
+		let currentOpacity = parseFloat(window.getComputedStyle(element).opacity);
+
+		// If we're not done
+		if (currentOpacity - opacity) {
+			
+			// Increment opacity
+			currentOpacity += opacityStep;
+
+			// Clamp opacity
+			currentOpacity = Clamp(currentOpacity);
+
+			// Apply opacity
+			element.style.opacity = currentOpacity;
+			
+			// Set display style
+			SetDisplay(element)
+		}
+		else {
+			// Apply final opacity
+			element.style.opacity = opacity;
+
+			// Set final display
+			SetDisplay(element);
+
+			// Remove interval
+			clearInterval(interval);
+
+			// Trigger Callback
+			if (completeCallback != null) {
+				completeCallback();
+			}
+		}
+
+	}, stepTime);
+
+	function SetDisplay(e) {
+		if (parseFloat(window.getComputedStyle(e).opacity) == 0) {
+			e.style.display = "none";
+		}
+		else {
+			e.style.display = "block";
+		}
+	}
+}
+
+/**
+ * Hides a given element
+ *
+ * @param element {element} the element to hide
+ *
+ * @return {void}
+ *
+ */
+function Hide(element) {
+	element.style.display = "none";
+	element.style.opacity = 0;
+}
+
+/**
+ * Clamps a number between 0 and 1
+ *
+ * @param num {number} the number to clamp
+ *
+ * @return {void}
+ *
+ */
+function Clamp(num) {
+
+	return Math.min(Math.max(num, 0), 1);
+}
+
+/**
+ * Runs a series of tests to see if IsItAPalindrome returns the expected responses
+ *
+ * @param displayOnPage {bool} if the results of the tests should be displayed on the page vs in the console (default = false)
+ *
+ * @return {bool} if IsItAPalidrome passed all the tests
+ *
+ */
+function Test(displayOnPage) {
+
+	if (displayOnPage == undefined) {
+		displayOnPage = false;
+	}
+
+	// Grab results element
+	let testResults = document.getElementById("test-results");
+	testResults.innerHTML = "";
+
+	// Create tests
+	let tests = [
+		{
+			phrase: "hello world",
+			expectedResult: false
+		},
+		{
+			phrase: "radar",
+			expectedResult: true
+		},
+		{
+			phrase: "a",
+			expectedResult: true
+		},
+		{
+			phrase: "aa",
+			expectedResult: true
+		},
+		{
+			phrase: "aaaaaaaa",
+			expectedResult: true
+		},
+		{
+			phrase: "aaaaaaaaa",
+			expectedResult: true
+		},
+		{
+			phrase: "",
+			expectedResult: true
+		},
+		{
+			phrase: " a a a     ",
+			expectedResult: true
+		},
+		{
+			phrase: "./?+=+?/ .",
+			expectedResult: true
+		},
+		{
+			phrase: "               ",
+			expectedResult: true
+		},
+		{
+			phrase: "1234567890",
+			expectedResult: false
+		}
+	]
+
+	// Get total number of tests
+	let numTests = tests.length;
+
+	// Container for incorrectly handled phrases
+	let incorrectlyHandled = [];
+	
+	// Run tests
+	for (let i = 0; i < numTests; i++) {
+		let currentTest = tests[i];
+
+		if (currentTest.expectedResult != IsItAPalindrome(currentTest.phrase)) {
+			// Hold on to phrases handled incorrectly
+			incorrectlyHandled.push(currentTest.phrase);
+		}
+	}
+
+	// Display the results on the page if applicable
+	if (displayOnPage) {
+		let pageResults = "";
+
+		pageResults += "Failed " + incorrectlyHandled.length + " / " + numTests + " tests:<br/>";
+		pageResults += "=================<br/>";
+		for (let i = 0; i < incorrectlyHandled.length; i++) {
+			pageResults += "" + (i+1) + ") \"" + incorrectlyHandled[i] + "\"<br/>";
+		}
+		pageResults += "=================<br/>";
+
+		testResults.innerHTML = pageResults;
+	}
+	else {
+		console.log("Failed " + incorrectlyHandled.length + " / " + numTests + " tests:");
+		console.log("=================");
+		for (let i = 0; i < incorrectlyHandled.length; i++) {
+			console.log("" + (i+1) + ") \"" + incorrectlyHandled[i] + "\"");
+		}
+		console.log("=================");
+	}
+
+	// Return if all tests passed
+	if (incorrectlyHandled.length == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/**
+ * Handles the setup of the page
+ *
+ * @return {void}
+ *
+ */
+$(document).ready(function(){
+
+	// Setup form submit event listener
+	let palidromeForm = document.getElementById("palindrome-form");
+	palidromeForm.addEventListener("submit", FormSubmitted);
+
+	// Setup test button click event listener
+	let testButton = document.getElementById("test-button");
+	testButton.addEventListener("click", function(){
+		Test(true);
+	});
 });
